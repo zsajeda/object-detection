e66861b7cd19edaf7dea252ceb8714d127f6592e
zsajeda <zsajeda30@gmail.com>
2024-12-12 02:14:06 -0800

    Add files via upload

---
+++ detectionobj.py
@@ -0,0 +1,27 @@
+Python 3.11.0 (main, Oct 24 2022, 18:26:48) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=========================== RESTART: D:\2271\sssss.py ==========================
+Traceback (most recent call last):
+  File "D:\2271\sssss.py", line 13, in <module>
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+cv2.error: OpenCV(4.10.0) D:\a\opencv-python\opencv-python\opencv\modules\imgproc\src\color.cpp:196: error: (-215:Assertion failed) !_src.empty() in function 'cv::cvtColor'
+
+>>> 
+=========================== RESTART: D:\2271\sssss.py ==========================
+Traceback (most recent call last):
+  File "D:\2271\sssss.py", line 13, in <module>
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+cv2.error: OpenCV(4.10.0) D:\a\opencv-python\opencv-python\opencv\modules\imgproc\src\color.cpp:196: error: (-215:Assertion failed) !_src.empty() in function 'cv::cvtColor'
+
+>>> 
+=========================== RESTART: D:\2271\sssss.py ==========================
+Traceback (most recent call last):
+  File "D:\2271\sssss.py", line 13, in <module>
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+cv2.error: OpenCV(4.10.0) D:\a\opencv-python\opencv-python\opencv\modules\imgproc\src\color.cpp:196: error: (-215:Assertion failed) !_src.empty() in function 'cv::cvtColor'
+
+>>> 
+=========================== RESTART: D:\2271\sssss.py ==========================
+>>> 
+=========================== RESTART: D:\2271\sssss.py ==========================
